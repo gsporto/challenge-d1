487ccdd4a9281fc85c9cecc28451d141b708be5f
Gabriel Porto <gabriel_porto@outlook.com>
2021-07-24 09:11:48 -0300

    add: styled components and assets

---
+++ src/styles/global.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+      font-family: 'Gotham-Book';
+      src: local('Gotham-Book'),
+          url('../assets/fonts/Gotham-Book.woff') format('woff'),
+          url('../assets/fonts/Gotham-Book.woff2') format('woff2');
+  }
+
+  @font-face {
+      font-family: 'Gotham-Bold';
+      src: local('Gotham-Bold'),
+          url('../assets/fonts/Gotham-Bold.woff') format('woff'),
+          url('../assets/fonts/Gotham-Bold.woff2') format('woff2');
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body{
+    background: #FAFBFF;
+    color: #3E4157;
+  }
+
+  body, input , button {
+    font-family: 'Gotham-Book',sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export { GlobalStyle };
