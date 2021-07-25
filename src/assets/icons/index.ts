c416269a7aa3ad3147c5f8744338288a5e7ad6e9
Gabriel Porto <gabriel_porto@outlook.com>
2021-07-25 18:13:23 -0300

    add: icons export

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,35 @@
+import { ReactComponent as BedIcon } from './bed.svg';
+import { ReactComponent as CcmCloudIcon } from './ccm-cloud.svg';
+import { ReactComponent as ChartIcon } from './chart-pie.svg';
+import { ReactComponent as CheckIcon } from './check.svg';
+import { ReactComponent as ExchangeIcon } from './exchange-alt.svg';
+import { ReactComponent as ExternalLinkIcon } from './external-link-square-alt.svg';
+import { ReactComponent as GemSolidIcon } from './gem-solid.svg';
+import { ReactComponent as PaperPlaneIcon } from './paper-plane.svg';
+import { ReactComponent as PenIcon } from './pen.svg';
+import { ReactComponent as PlayCircleIcon } from './play-circle.svg';
+import { ReactComponent as PlusIcon } from './plus.svg';
+import { ReactComponent as RocketIcon } from './rocket.svg';
+import { ReactComponent as SignOutIcon } from './sign-out-alt.svg';
+import { ReactComponent as TableIcon } from './table.svg';
+import { ReactComponent as ToolsIcon } from './tools.svg';
+import { ReactComponent as UserFriendsIcon } from './user-friends.svg';
+
+export {
+  BedIcon,
+  CcmCloudIcon,
+  ChartIcon,
+  CheckIcon,
+  ExchangeIcon,
+  ExternalLinkIcon,
+  GemSolidIcon,
+  PaperPlaneIcon,
+  PenIcon,
+  PlayCircleIcon,
+  PlusIcon,
+  RocketIcon,
+  SignOutIcon,
+  TableIcon,
+  ToolsIcon,
+  UserFriendsIcon,
+};
