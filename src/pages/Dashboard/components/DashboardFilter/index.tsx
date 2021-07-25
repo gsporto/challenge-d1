75da286fcf6847876adb926899cbca70e7d095a5
Gabriel Porto <gabriel_porto@outlook.com>
2021-07-25 19:24:03 -0300

    add: async filters

--- src/pages/Dashboard/components/DashboardFilter/index.tsx
+++ src/pages/Dashboard/components/DashboardFilter/index.tsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from 'react';
 import { TableIcon } from 'assets/icons';
 import { BadgeCircular } from 'components/BadgeCircular';
+import { IFilter } from 'dtos/Dashboard';
+import { getFilter } from 'services/Dashboard';
 import { Container, ItemFilter } from './styles';
 
-const DashboardFilter = () => (
-  <Container>
-    <ItemFilter>
-      <TableIcon />
-      Todas
-      <BadgeCircular size={22}>22</BadgeCircular>
-    </ItemFilter>
-  </Container>
-);
+const DashboardFilter = () => {
+  const [filters, setFilters] = useState<IFilter[]>();
+
+  useEffect(() => {
+    const async = async () => {
+      try {
+        const response = await getFilter();
+        setFilters(response.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    async();
+  }, []);
+
+  return (
+    <Container>
+      {filters?.map(({ id, name, quantity }) => (
+        <ItemFilter key={id}>
+          <TableIcon />
+          {name}
+          <BadgeCircular size={22}>{quantity}</BadgeCircular>
+        </ItemFilter>
+      ))}
+    </Container>
+  );
+};
 
 export { DashboardFilter };
