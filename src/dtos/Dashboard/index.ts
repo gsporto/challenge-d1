c23d9368387e1ea3a15bc9b2b56a6b92f64851f9
Gabriel Porto <gabriel_porto@outlook.com>
2021-07-25 19:10:08 -0300

    add: dto

---
+++ src/dtos/Dashboard/index.ts
@@ -0,0 +1,15 @@
+export interface IFilter {
+  name: string;
+  status: number;
+  recipients: string;
+  success: string;
+  id: string;
+}
+
+export interface IJourney {
+  name: string;
+  status: number;
+  recipients: string;
+  success: string;
+  id: string;
+}
